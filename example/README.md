c0cf6ac4e88bf31fb0e72f32c1bcd5a8f93d32bc
Paul Young <paulyoungonline@gmail.com>
2018-05-16 21:37:52 -0700

    First version (#1)
    * First version
    
    * Depend on purescript-style ^0.1.0

---
+++ example/README.md
@@ -0,0 +1,11 @@
+# Example
+
+This example is equivalent to the one found in the [README](https://github.com/styled-components/styled-components#example) of `styled-components`.
+
+## Building
+
+```sh
+$ bower install
+$ pulp build --to index.js
+$ open index.html
+```
---
+++ src/Styled/Components.purs
@@ -0,0 +1,29 @@
+module Styled.Components
+  ( element
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Halogen.HTML as HH
+import Halogen.HTML.Core as HC
+import Halogen.HTML.Properties as HP
+import Style.Property (Property)
+import Style.Render (inline)
+
+element
+  :: forall s r p i
+   . (Array (HH.IProp r i) -> Array (HH.HTML p i) -> HH.HTML p i)
+  -> Array (s -> Array Property)
+  -> s
+  -> Array (HH.IProp r i)
+  -> Array (HH.HTML p i)
+  -> HH.HTML p i
+element el fns state props = el (props <> [ attr ])
+  where
+
+  styles :: Array Property
+  styles = Array.foldl (\styleProps fn -> styleProps <> fn state) [] fns
+
+  attr :: HH.IProp r i
+  attr = HP.attr (HC.AttrName "style") $ inline styles
